ea23eb79f7c812ddb798e60a589094a9879b702b
rchavarria <txingo@gmail.com>
2013-09-12 10:42:46 +0200

    added code to the servlet to read request parameters and output them as a response

--- request-parameters/src/main/java/es/rchavarria/webapp/RequestParametersServletDemo.java
+++ request-parameters/src/main/java/es/rchavarria/webapp/RequestParametersServletDemo.java
@@ -2,8 +2,10 @@ package es.rchavarria.webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.annotation.WebServlet;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,11 +20,26 @@ public class RequestParametersServletDemo extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        Enumeration<String> names = request.getParameterNames();
+
         PrintWriter out = response.getWriter();
         try{
+        	String yes = names.hasMoreElements() ? "will" : "won't";
             out.println("<h2>");
-            out.println("This servlet will read your form parameters");
+            out.println("This servlet " + yes + " read your form parameters");
             out.println("</h2>");
+            
+            out.println("<ul>");
+            while(names.hasMoreElements()) {
+            	String name = names.nextElement();
+            	String value = request.getParameter(name);
+            	
+            	out.println("<li>");
+                out.println(name + ": " + value);
+                out.println("</li>");
+            }
+            out.println("</ul>");
+            
         } finally {
             out.close();
         }
