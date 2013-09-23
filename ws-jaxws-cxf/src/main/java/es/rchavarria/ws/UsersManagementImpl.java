62cbfbe0e40747e9faf14a8289ff3c4c326efb79
rchavarria <txingo@gmail.com>
2013-09-23 12:36:56 +0200

    green

--- ws-jaxws-cxf/src/main/java/es/rchavarria/ws/UsersManagementImpl.java
+++ ws-jaxws-cxf/src/main/java/es/rchavarria/ws/UsersManagementImpl.java
@@ -1,17 +1,24 @@
 package es.rchavarria.ws;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "es.rchavarria.ws.UsersManagement",
             serviceName = "Users")
 public class UsersManagementImpl implements UsersManagement {
 
+	private List<String> users = new LinkedList<String>();
+	
 	public String getUser(int userId) {
 		return "foo bar user";
 	}
 
 	public int addUser(String name) {
-		return 42;
+		users.add(name);
+		
+		return users.size();
 	}
  
 }
