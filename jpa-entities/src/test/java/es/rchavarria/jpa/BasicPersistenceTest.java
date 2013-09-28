bdfb02dd9922f6f3d6e34a0fdaa526279e9884f8
rchavarria <txingo@gmail.com>
2013-09-28 12:06:47 +0200

    create unit test to test basic persistence. in red up to now

---
+++ jpa-entities/src/test/java/es/rchavarria/jpa/BasicPersistenceTest.java
@@ -0,0 +1,14 @@
+package es.rchavarria.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicPersistenceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
