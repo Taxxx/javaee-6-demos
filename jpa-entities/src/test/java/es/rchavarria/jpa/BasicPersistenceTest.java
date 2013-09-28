6a28bf2d8ad0c8199d26508a1f4a3be296fa650b
rchavarria <txingo@gmail.com>
2013-09-28 12:21:24 +0200

    implement the first test. all code is in only one method

--- jpa-entities/src/test/java/es/rchavarria/jpa/BasicPersistenceTest.java
+++ jpa-entities/src/test/java/es/rchavarria/jpa/BasicPersistenceTest.java
@@ -2,13 +2,36 @@ package es.rchavarria.jpa;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
 
 public class BasicPersistenceTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Person p = new Person();
+		p.setName("Foo bar");
+		p.setAge(19);
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(p);
+		tx.commit();
+		
+		tx.begin();
+		Person pp = em.find(Person.class, new Long(1));
+		assertEquals(p.getName(), pp.getName());
+		tx.commit();
+
+		em.close();
+		emf.close();
 	}
 
 }
