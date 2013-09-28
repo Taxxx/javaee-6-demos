a6ad2dcda856e2796340cf761f140b6721c596f3
rchavarria <txingo@gmail.com>
2013-09-28 12:28:19 +0200

    refactor test to create entity manager factory, entity manager and transaction in the setUp method and close them in the tearDown one

--- jpa-entities/src/test/java/es/rchavarria/jpa/BasicPersistenceTest.java
+++ jpa-entities/src/test/java/es/rchavarria/jpa/BasicPersistenceTest.java
@@ -1,37 +1,55 @@
 package es.rchavarria.jpa;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BasicPersistenceTest {
 
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private EntityTransaction tx;
+	
+	@Before
+	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("test-jpa");
+		em = emf.createEntityManager();
+
+		tx = em.getTransaction();
+		tx.begin();
+	}
+	
+	@After
+	public void tearDown() {
+		tx.commit();
+		em.close();
+		emf.close();
+	}
+	
 	@Test
 	public void test() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test-jpa");
-		EntityManager em = emf.createEntityManager();
-		
-		Person p = new Person();
-		p.setName("Foo bar");
-		p.setAge(19);
-		
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+		Person p = createPerson();
 		em.persist(p);
 		tx.commit();
 		
 		tx.begin();
 		Person pp = em.find(Person.class, new Long(1));
 		assertEquals(p.getName(), pp.getName());
-		tx.commit();
+	}
 
-		em.close();
-		emf.close();
+	private Person createPerson() {
+		Person p = new Person();
+		p.setName("Foo bar");
+		p.setAge(19);
+		
+		return p;
 	}
 
 }
