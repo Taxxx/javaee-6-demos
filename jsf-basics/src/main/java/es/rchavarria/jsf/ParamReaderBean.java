cab98f75524bd522e56bdfae17f5704d8c439795
rchavarria <txingo@gmail.com>
2013-10-25 14:27:11 +0200

    use tomahack to handle a list of strings and output them to the web page

--- jsf-basics/src/main/java/es/rchavarria/jsf/ParamReaderBean.java
+++ jsf-basics/src/main/java/es/rchavarria/jsf/ParamReaderBean.java
@@ -1,6 +1,13 @@
 package es.rchavarria.jsf;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 @ManagedBean(name = "paramReader", eager = true)
 public class ParamReaderBean {
@@ -9,4 +16,28 @@ public class ParamReaderBean {
 		return "Here is the result of reading parameters";
 	}
 
+	public List<Parameter> getParams() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) fc.getExternalContext().getRequest();
+		
+		Enumeration<String> names = request.getParameterNames();
+		if(!names.hasMoreElements()) return Collections.emptyList();
+
+		List<Parameter> params = new LinkedList<Parameter>();
+		while(names.hasMoreElements()) {
+			String name = names.nextElement();
+			String value = request.getParameter(name);
+			Parameter p = new Parameter();
+			p.setKey(name);
+			p.setValue(value);
+
+			params.add(p);
+		}
+		
+		return params;
+	}
+	
+	public String login() {
+		return "home";
+	}
 }
